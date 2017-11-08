be12bd7c5e761149463480e39d4b7b2585c6264b
y13423685271 <y362424>
2017-11-08 10:30:12 +0800

    ini

--- mybatis/src/main/java/com/example/demo/entity/User.java
+++ mybatis/src/main/java/com/example/demo/entity/User.java
@@ -2,15 +2,13 @@ package com.example.demo.entity;
 
 import java.util.Date;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
- * 用户信息
- * Created by Infi on 17/3/22.
+ * 用户信息 Created by Infi on 17/3/22.
  */
 @Data
 public class User {
+
     private Long id;
     private String name;
     private Date birthday;
