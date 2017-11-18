81de9450bc70afa8799064c524573a54b566d941
y13423685271 <y362424>
2017-11-18 18:41:29 +0800

    ini

--- mybatis/src/main/java/com/example/demo/MybatisApplication.java
+++ mybatis/src/main/java/com/example/demo/MybatisApplication.java
@@ -2,10 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MybatisApplication {
 
     public static void main(String[] args) {
