61af9a70473943dc627ff93ef2f2d876b29dfa21
y13423685271 <y362424>
2017-11-27 14:42:48 +0800

    ini

---
+++ druid/src/main/java/com/jack/DruidApplication.java
@@ -0,0 +1,16 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangyueming
+ */
+@SpringBootApplication
+public class DruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidApplication.class, args);
+    }
+}
+
