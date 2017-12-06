70b9f4684c0d800d2040110e97ce4b3d70610870
y13423685271 <y362424>
2017-12-06 20:01:10 +0800

    ini

---
+++ thymeleaf/src/main/java/com/jack/thymeleaf/entity/Person.java
@@ -0,0 +1,17 @@
+package com.jack.thymeleaf.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author yangyueming
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+    private Integer age;
+}
