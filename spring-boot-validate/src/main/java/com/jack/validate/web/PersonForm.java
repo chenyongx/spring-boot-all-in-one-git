a51ca81920e921b35a863bf649eac627da0dae4a
y13423685271 <y362424>
2018-01-03 18:10:32 +0800

    refactor

---
+++ spring-boot-validate/src/main/java/com/jack/validate/web/PersonForm.java
@@ -0,0 +1,18 @@
+package com.jack.validate.web;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PersonForm {
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+}
