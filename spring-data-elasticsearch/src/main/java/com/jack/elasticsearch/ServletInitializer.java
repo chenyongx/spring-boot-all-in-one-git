ef3f6f6862c17fa73aaa5a0244e0511a408f8eee
yangyueming <123>
2018-10-05 17:31:11 +0800

    init

--- spring-data-elasticsearch/src/main/java/com/jack/elasticsearch/ServletInitializer.java
+++ spring-data-elasticsearch/src/main/java/com/jack/elasticsearch/ServletInitializer.java
@@ -1,16 +1,13 @@
 package com.jack.elasticsearch;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 /**
  * @author yangyueming
  */
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ElasticSearchApplication.class);
-	}
-
-}
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(ElasticSearchApplication.class);
+//	}
+//
+//}
