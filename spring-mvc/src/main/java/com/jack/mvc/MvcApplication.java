a0b14a896b26156f0693b9a633c26e623dcc96cf
yangyueming <123>
2018-11-17 19:24:59 +0800

    init

--- spring-mvc/src/main/java/com/jack/mvc/MvcApplication.java
+++ spring-mvc/src/main/java/com/jack/mvc/MvcApplication.java
@@ -2,6 +2,8 @@ package com.jack.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -13,5 +15,6 @@ public class MvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MvcApplication.class, args);
+        ApplicationContext context = new ClassPathXmlApplicationContext("services.xml", "daos.xml");
     }
 }
